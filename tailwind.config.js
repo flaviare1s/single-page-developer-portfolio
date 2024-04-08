d50c83e2b02ce1e77a91fd2b116787107a4a3492
Flávia Reis <flaviarster@gmail.com>
2024-04-08 18:13:53 -0300

    I added custom font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Space_Grotesk': ['Space Grotesk', 'sans-serif'],
+      },
+      colors: {
+        dark_black: '#151515',
+        neon_green: '#4EE1A0',
+        dark_gray: '#242424',
+        cream: '#D9D9D9',
+        pure_white:'#FFFFFF'
+      },
+    },
   },
   plugins: [],
 }
